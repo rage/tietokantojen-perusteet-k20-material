31cd31741602875be825037a1e39bc17e77119e1
Henrik Nygren <nygrenh@gmail.com>
2019-03-12 10:00:31 +0200

    Update quizzes

--- src/components/EndOfSubSection.js
+++ src/components/EndOfSubSection.js
@@ -92,7 +92,8 @@ class EndOfSubSection extends React.Component {
               )}
               <p>
                 Muistathan tarkistaa pistetilanteesi materiaalin oikeassa
-                alareunassa olevasta pallosta! Pistilanne ilmestyy materiaaliin myöhemmin.
+                alareunassa olevasta pallosta! Pistilanne ilmestyy materiaaliin
+                myöhemmin.
               </p>
             </div>
           )
