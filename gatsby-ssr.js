7951f433f1da0358c6391aff55b14cb0714f83fc
Henrik Nygren <nygrenh@gmail.com>
2019-08-20 13:50:26 +0300

    Remove getPageContext

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,15 +10,12 @@
 const React = require('react')
 const { renderToString } = require('react-dom/server')
 const JssProvider = require('react-jss/lib/JssProvider').default
-const getPageContext = require('./src/getPageContext').default
 
 function replaceRenderer({
   bodyComponent,
   replaceBodyHTMLString,
-  setHeadComponents,
 }) {
   // Get the context of the page to collected side effects.
-  const muiPageContext = getPageContext()
 
   const bodyHTML = renderToString(
     <JssProvider registry={muiPageContext.sheetsRegistry}>
@@ -27,16 +24,6 @@ function replaceRenderer({
   )
 
   replaceBodyHTMLString(bodyHTML)
-  setHeadComponents([
-    <style
-      type="text/css"
-      id="jss-server-side"
-      key="jss-server-side"
-      dangerouslySetInnerHTML={{
-        __html: muiPageContext.sheetsRegistry.toString(),
-      }}
-    />,
-  ])
 }
 
 exports.replaceRenderer = replaceRenderer
