30a79a1001290a1744ce66d52fdff08bf33c0e73
Henrik Nygren <nygrenh@gmail.com>
2019-08-20 13:54:57 +0300

    Remove custom ssr logic

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,34 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-/* eslint-disable react/no-danger */
-
-const React = require('react')
-const { renderToString } = require('react-dom/server')
-const JssProvider = require('react-jss/lib/JssProvider').default
-
-function replaceRenderer({
-  bodyComponent,
-  replaceBodyHTMLString,
-}) {
-  // Get the context of the page to collected side effects.
-
-  const bodyHTML = renderToString(
-    <JssProvider registry={muiPageContext.sheetsRegistry}>
-      {bodyComponent}
-    </JssProvider>
-  )
-
-  replaceBodyHTMLString(bodyHTML)
-}
-
-exports.replaceRenderer = replaceRenderer
-
-// It's not ready yet: https://github.com/gatsbyjs/gatsby/issues/8237.
-//
-// const withRoot = require('./src/withRoot').default;
-// const WithRoot = withRoot(props => props.children);
-
-// exports.wrapRootElement = ({ element }) => {
-//   return <WithRoot>{element}</WithRoot>;
-// };
