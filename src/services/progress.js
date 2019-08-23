0c211e267bc499336b39ede08751a6d1b6a57c08
Henrik Nygren <nygrenh@gmail.com>
2019-08-23 11:34:54 +0300

    Don't visualize points from weeks without programming

--- src/services/progress.js
+++ src/services/progress.js
@@ -21,14 +21,14 @@ export async function fetchProgress() {
       })
     },
   )
-  // const toBeDeleted = []
-  // Object.entries(progressByGroup).forEach(([group, serviceEntries]) => {
-  //   if (!Object.keys(serviceEntries).find(o => o === "Ohjelmointitehtävät")) {
-  //     toBeDeleted.push(group)
-  //   }
-  // })
-  // toBeDeleted.forEach(o => {
-  //   delete progressByGroup[o]
-  // })
+  const toBeDeleted = []
+  Object.entries(progressByGroup).forEach(([group, serviceEntries]) => {
+    if (!Object.keys(serviceEntries).find(o => o === "Ohjelmointitehtävät")) {
+      toBeDeleted.push(group)
+    }
+  })
+  toBeDeleted.forEach(o => {
+    delete progressByGroup[o]
+  })
   return progressByGroup
 }
